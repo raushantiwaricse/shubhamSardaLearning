939c8c6a69f840736d4b5b94c21c3d163ed54f84
raushantiwaricse <raushantiwaricse@gmail.com>
2023-02-27 05:01:33 +0530

    task list completed with delete , toggle feature27022023

--- src/App.js
+++ src/App.js
@@ -6,17 +6,32 @@ function App() {
     { id: 5412, name: "Edit lecture", completed: true },
     { id: 7458, name: "watch lecture", completed: false },
   ]);
+  const [show, setShow] = useState(false);
+  function handleDelete(id) {
+    console.log(id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
   return (
     <div className="App">
       <h1>Task List</h1>
       <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <span>
-              {task.id} - {task.name}
-            </span>
-          </li>
-        ))}
+        <button onClick={() => setShow(!show)} className="trigger">
+          Toggle
+        </button>
+        {show &&
+          tasks.map((task) => (
+            <li
+              key={task.id}
+              className={task.completed ? "completed" : "incomplte"}
+            >
+              <span>
+                {task.id} - {task.name}
+              </span>
+              <button onClick={() => handleDelete(task.id)} className="delete">
+                Delete
+              </button>
+            </li>
+          ))}
       </ul>
     </div>
   );
